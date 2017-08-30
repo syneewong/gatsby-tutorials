b11bdb7ccffc888b8a196aca7b02765eefd5821e
Syn-Ee Wong <synee.wong@ie.com.au>
2017-08-30 17:16:48 +1000

    Link up index page.

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import g from "glamorous"
+import Link from "gatsby-link"
 
 import { rhythm } from "../utils/typography"
 
@@ -15,13 +16,18 @@ export default ({ data }) => {
       </h4>
       {data.allMarkdownRemark.edges.map(({ node }) =>
         <div>
-          <g.H3 marginBottom={rhythm(1 / 4)}>
-            {node.frontmatter.title}{" "}
-            <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
-          </g.H3>
-          <p>
-            {node.excerpt}
-          </p>
+        	<Link
+        		to={node.fields.slug}
+        		css={{ textDecoration: `none`, color: `inherit` }}
+        	>
+	          <g.H3 marginBottom={rhythm(1 / 4)}>
+	            {node.frontmatter.title}{" "}
+	            <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
+	          </g.H3>
+	          <p>
+	            {node.excerpt}
+	          </p>
+	        </Link>
         </div>
       )}
     </div>
@@ -41,6 +47,9 @@ export const query = graphql`
       totalCount
       edges {
         node {
+        	fields {
+        		slug
+        	}
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
