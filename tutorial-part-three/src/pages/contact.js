a2ae79158f00ed0a3c12dfeb53b806d29acbd770
Syn-Ee Wong <synee.wong@ie.com.au>
2017-09-01 10:11:25 +1000

    Update contact link `mailto:`.

--- tutorial-part-three/src/pages/contact.js
+++ tutorial-part-three/src/pages/contact.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Link from "gatsby-link"
 
 export default () =>
   <div>
     <h1>I'd love to talk! Email me at the address below</h1>
     <p>
-      <a mailto="me@example.com">me@example.com</a>
+      <a href="mailto:me@example.com">me@example.com</a>
     </p>
   </div>
